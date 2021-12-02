1cfeca569528f1d8d5f9b27eaadb29e4442802d8
Alldarknex <1201.saulamendezo@gmail.com>
2021-12-02 16:47:25 -0600

    egundo commit

--- sw.js
+++ sw.js
@@ -10,18 +10,18 @@ self.addEventListener('install', evento=>{
     const promesa =caches.open(CACHE)
         .then(cache=>{
             return cache.addAll([
-                '/',
-                '/index.html',
+                // '/',
+                'index.html',
                 'css/bootstrap.css',
                 'css/bootstrap.css.map',
                 'css/personal.css',
                 'css/icons.css',
                 'js/bootstrap.min.js',
                 'js/app.js',
-                '/offline.html',
+                'offline.html',
                 'font-awesome/css/font-awesome.min.css',
                 'img/phone.jpg.png',
-                '/contacto.html',
+                'contacto.html',
                 'img/gs.png',
                 'img/ImpvlsiveLogo.png',
                 'img/marketing.png',
